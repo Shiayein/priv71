696b362443bc6d43e5cf9fd7e5dea51229211ce5
Coemprosad <shiayeiny@gmail.com>
2025-10-16 12:27:43 +0200

    Update key_system.lua

--- key_system.lua
+++ key_system.lua
@@ -10,9 +10,9 @@ KeyStore.DEV_KEYS = {
   "83TE-8IYQ-ES31-EFK6" -- Your only key
 }
 
--- Key bindings with HWID
+-- Key bindings with HWID and users (empty by default)
 KeyStore.KEYS = {
-  ["83TE-8IYQ-ES31-EFK6"] = { hwid = "1B830866-70FD-4F14-9AF2-0603C4741363" } -- Your HWID
+  ["83TE-8IYQ-ES31-EFK6"] = { users = {}, hwid = "1B830866-70FD-4F14-9AF2-0603C4741363" } -- Your updated HWID
 }
 
 return KeyStore

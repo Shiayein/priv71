ac70898917b6f2c957fc9777f76ec571ef26f3f5
Coemprosad <shiayeiny@gmail.com>
2025-10-16 15:01:30 +0200

    Update ragebot.lua

--- ragebot.lua
+++ ragebot.lua
@@ -1,17 +1,23 @@
 -- ragebot.lua
 local module = {}
-local ragebotEnabled = false
+local ragebotActive = false
+local Services = game:GetService("Players")
 
 function module.init()
     print("[RAGEBOT] Ragebot module loaded!")
     -- Vérifier les variables globales définies par gui_extensions.lua
     if getgenv().PlayerActions then
-        -- Simuler une réaction au toggle (logique à développer)
-        getgenv().PlayerActions:GetToggle("Ragebot Enabled"):OnChanged(function(Value)
-            ragebotEnabled = Value
-            print("[RAGEBOT] Ragebot toggled to: " .. tostring(ragebotEnabled))
-            -- Logique ragebot/autokill à implémenter ici
-        end)
+        -- Connecter le toggle Ragebot Active
+        local toggle = getgenv().PlayerActions:GetToggle("Ragebot Active")
+        if toggle then
+            toggle:OnChanged(function(state)
+                ragebotActive = state
+                print("[RAGEBOT] Ragebot toggled to: " .. tostring(ragebotActive))
+                -- Logique à implémenter ici
+            end)
+        else
+            warn("[RAGEBOT] Ragebot Active toggle not found!")
+        end
     else
         warn("[RAGEBOT] PlayerActions not found, ragebot controls unavailable!")
     end
@@ -19,9 +25,10 @@ end
 
 -- Fonction placeholder pour le ragebot (à compléter)
 function module.update()
-    if ragebotEnabled then
-        -- Logique à implémenter : Détection des joueurs, attaque automatique, etc.
+    if ragebotActive then
+        -- Logique à implémenter : Détection des joueurs proches, attaque automatique, etc.
         print("[RAGEBOT] Updating... (Logic not implemented yet)")
+        -- Exemple futur : Chercher un joueur cible et attaquer
     end
 end
 
